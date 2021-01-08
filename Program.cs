b8dccedc11817886dd0133ee6f302e1bc9c62a10
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-08 09:25:50 -0500

    NewVariables

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 
 namespace NEWVariables
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var numberOfCupsOfCoffee = 1;
+            var fullName = "Erika Stigleman";
+            var today = DateTime.Now;
+
+            Console.WriteLine($"Hello! My Name is {fullName}, on {today} I enjoyed {numberOfCupsOfCoffee} cup of coffee.");
+
+
+            var userName = Console.ReadLine();
+
+
+        }
     }
-  }
 }
